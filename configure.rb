12252630596144a77963098f3b9a1193c866ad23
Eduardo Delgado <alu0100450699@ull.edu.es>
2013-09-25 13:09:13 +0100

    eliminando ficheros antiguos

--- configure.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key       = 'WSTyn6j5KaMNIPLqb2IcaA'
-  config.consumer_secret    = 'eHebG9vagBnu5tUmGBu7o9vILQFdfj0NGtdAoxChWug'
-  config.oauth_token        = "378627847-m9Y6LhfezkYWDGgQwXYwQwSuijCEN0dpRI4xKRTw"
-  config.oauth_token_secret = "Fi6TTDzMzCz07VrMG9uks3HifVUqj82q1PNLGYd1xNE"
-end
